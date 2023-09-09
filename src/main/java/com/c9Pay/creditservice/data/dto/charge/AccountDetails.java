8f2878970340e722f0d4c903ae697c9546cd52fd
minjong <koust6u@pusan.ac.kr>
2023-09-10 08:45:00 +0900

    feat: schema로 등록 및, 역할에 맞게 Controller 분리
    * schema로 등록 및, 역할에 맞게 Controller 분리 ---

--- src/main/java/com/c9Pay/creditservice/data/dto/charge/AccountDetails.java
+++ src/main/java/com/c9Pay/creditservice/data/dto/charge/AccountDetails.java
@@ -1,5 +1,6 @@
 package com.c9Pay.creditservice.data.dto.charge;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -8,12 +9,15 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Schema(description = "계좌 조회 응답 형식")
 @AllArgsConstructor
 public class AccountDetails {
 
+    @Schema(description = "계좌 크레딧 금액")
     @Min(0)
     Long credit;
 
+    @Schema(description = "개인 식별 번호")
     @NotBlank
     String serialNumber;
 }
