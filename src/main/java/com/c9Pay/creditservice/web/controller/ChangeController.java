158424bfbc60fffc114df13ef810ac6fdfb613cc
minjong <koust6u@pusan.ac.kr>
2023-09-10 17:22:27 +0900

    fix: 계좌 충선 버그 수정
    * end point  잘못된 설정으로 인해 수정 ---

--- src/main/java/com/c9Pay/creditservice/web/controller/ChangeController.java
+++ src/main/java/com/c9Pay/creditservice/web/controller/ChangeController.java
@@ -10,14 +10,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/account")
 public class ChangeController implements ChangeControllerDocs {
 
     private final AccountService accountService;
